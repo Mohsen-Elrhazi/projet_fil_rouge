e10d60a8d1e0864f1ac0057b1f605df1afd44891
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-03-23 22:19:52 +0000

    créer request LoginRequest pour validation les données

---
+++ backend/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email:dns,rfc',
+            'password' => 'required|',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'email.required' => 'L\'email est obligatoire.',
+            'email.email' => 'Veuillez fournir une adresse email valide.',
+            'password.required' => 'Le mot de passe est obligatoire.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'message' => $validator->errors(),
+        ], 422));
+    }
+}
